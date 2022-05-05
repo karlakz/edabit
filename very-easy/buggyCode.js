3cb287203fe8730851500ee03da4dfb6b8c95d95
karlakz <yakiyaeva@gmail.com>
2022-05-05 22:48:48 +0600

    [file]: Add a function that fixes the syntax error

---
+++ very-easy/buggyCode.js
@@ -0,0 +1,12 @@
+// https://edabit.com/challenge/j7yQbF3J3rToHsDBP
+
+// Reference
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Exponentiation
+
+// function cubes(a) {
+	// retunr a ** 3
+// }
+
+function cubes(a) {
+	return a ** 3;
+}
