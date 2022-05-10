7cf6b2461da6e19b50362b47656a31d531af9de0
karlakz <yakiyaeva@gmail.com>
2022-05-10 21:29:19 +0600

    [file]: Add a function that returns true if an argument is less than or equal to zero

---
+++ very-easy/numLessOrEqualZero.test.js
@@ -0,0 +1,13 @@
+const numLessOrEqualZero = require('./numLessOrEqualZero');
+
+test('Return true if a number is less than or equal to 0', () => {
+    expect(numLessOrEqualZero(0)).toBe(true);
+});
+
+test('Return true if a number is less than or equal to 0', () => {
+    expect(numLessOrEqualZero(5)).toBe(false);
+});
+
+test('Return true if a number is less than or equal to 0', () => {
+    expect(numLessOrEqualZero(-5)).toBe(true);
+});
