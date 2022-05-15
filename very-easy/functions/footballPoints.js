0b718f09f0cc1e5ce4016b2bda1af7a64b73fc13
karlakz <yakiyaeva@gmail.com>
2022-05-15 23:20:18 +0600

    [refactor]: Add task reference for fotballPoints.js file

--- very-easy/functions/footballPoints.js
+++ very-easy/functions/footballPoints.js
@@ -1,3 +1,4 @@
+// Task: https://edabit.com/challenge/GwvwXHWCThHZrR7xu
 function footballPoints(wins, draws, losses) {
     if(wins < 0 || draws < 0 || losses < 0) {
         return 'Please, enter positive integers';
